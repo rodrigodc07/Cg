3510b2c7b04d960cda4791df56dd4de2ab2948b8
rodrigodc07 <31592221+rodrigodc07@users.noreply.github.com>
2018-11-06 11:10:03 -0200

    Função recebe angulo em graus

--- ex1.m
+++ ex1.m
@@ -1,7 +1,8 @@
-function ex1(mat,teta)
+function ex1(mat,graus)
   [cx,cy] = calc_center(mat)
   trans = [1 0 -cx; 0 1 -cy; 0 0 1];
   trans_v = inv(trans);
+  teta = graus * pi/180;
   rot = [cos(teta) -sin(teta) 0 ;sin(teta) cos(teta) 0; 0 0 1];
   full_transform =  trans_v * rot * trans;
   while (true)
