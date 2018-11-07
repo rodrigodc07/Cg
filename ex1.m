3493d96b1090372c53059082e6187ce7ec3e9af1
Rodrigo Cruz <rodrigodc07@gmail.com>
2018-11-07 10:20:32 -0200

    Faz a rotação apenas uma vez e apresenta as figuras antes e dps da rotação

--- ex1.m
+++ ex1.m
@@ -1,13 +1,12 @@
 function ex1(mat,graus)
+  print_mat(mat)
   [cx,cy] = calc_center(mat);
   trans = [1 0 -cx; 0 1 -cy; 0 0 1];
   trans_v = inv(trans);
   teta = graus * pi/180;
   rot = [cos(teta) -sin(teta) 0 ;sin(teta) cos(teta) 0; 0 0 1];
   full_transform =  trans_v * rot * trans;
-  while (true)
-    mat = apply_transform(mat,full_transform);
-    print_mat(mat)
-    sleep(0.1)
-  endwhile
+  nova_mat = apply_transform(mat,full_transform);
+  figure()
+  print_mat(nova_mat)
 endfunction
