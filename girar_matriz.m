2e51a9bd390d69beb3ef39cebdbd09f00772da9b
Rodrigo Cruz <rodrigodc07@gmail.com>
2018-11-07 09:52:05 -0200

    remove girar

--- girar_matriz.m
+++
@@ -1,9 +0,0 @@
-function ex1(mat,teta)
-  rot = [cos(teta) -sin(teta) 0 ;sin(teta) cos(teta) 0; 0 0 1];
-  nq = mat;
-  while (true)
-    nq = apply_transform(nq,rot);
-    print_mat(nq)
-    sleep(0.1)
-  endwhile
-endfunction
