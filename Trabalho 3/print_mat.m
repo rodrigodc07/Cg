84752ce2f128b484e8ee4a7dda7d6a466a77c8df
Rodrigo Cruz <rodrigo.cruz@b2wdigital.com>
2018-11-27 09:39:41 -0200

    remove print nao utilizado

--- Trabalho 3/print_mat.m
+++
@@ -1,9 +0,0 @@
-function print_mat(q)
-  clf;
-  axis([-3 9 -3 9],'square');
-  [dim,qp] = size(q);
-  for i = 1:qp-1
-    line([q(1,i),q(1,i+1)],[q(2,i),q(2,i+1)])
-  endfor
-  line([q(1,1),q(1,qp)],[q(2,1),q(2,qp)])
-endfunction
