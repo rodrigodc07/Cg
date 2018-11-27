f840ffe6caec4f90453fb92d9ef9150391b0539c
Rodrigo Dantas <rodrigodc07@gmail.com>
2018-11-26 22:53:37 -0200

    Termina trabalho3

---
+++ Trabalho 3/Ex3.m
@@ -0,0 +1,32 @@
+original = [135 493 520 111; 12 17 271 267; 1 1 1 1];
+transformada = [1 300 300 1; 1 1 300 300; 1 1 1 1]; 
+
+placa = imread('Imagens Teste-20181121/Placa_de_Trânsito_PARE.jpg');
+figure("Name","Imagem Original");
+imshow(placa);
+
+T = inv(find_transform(original,transformada));
+new_img_1 = zeros(300,300,3,"uint8");
+new_img_2 = zeros(300,300,3,"uint8");
+
+for c = 1:300 
+	for l = 1:300
+		pt = T * [ c; l; 1];
+		x = pt(1)/pt(3);
+		y = pt(2)/pt(3);
+		for i = 1:3
+			new_img_1(l,c,i) = placa(round(y),round(x),i);
+      w1 = placa(floor(y), floor(x), i) * (abs(x-floor(x))) * (abs(y-floor(y)));
+      w2 = placa(ceil(y), floor(x), i)* (abs(x-floor(x)))* (abs(y-ceil(y)));
+      w3 = placa(ceil(y), ceil(x), i)* (abs(x-ceil(x)))* (abs(y-ceil(y)));
+      w4 = placa(floor(y), ceil(x), i)* (abs(x-ceil(x)))* (abs(y-floor(y)));
+      new_img_2(l,c,i) = w1 + w2 + w3 + w4;
+		endfor
+	endfor		
+endfor
+
+figure("Name","Imagem com núcleo constante");
+imshow(new_img_1);
+
+figure("Name","Imagem com núcleo trianguar");
+imshow(new_img_2);
--- Trabalho 3/find_transform.m
+++ Trabalho 3/find_transform.m
@@ -1,17 +1,17 @@
-original = [0,0;0 1; 1 1; 1 0];
-transformada = [2,1;3 5; 6 6; 7 2];
-[lin,col] = size(transformada);
-L = reshape(transformada',lin*col,1);
-A = zeros(col,8);
-for i = 1:lin
- x = original(i,1);
- y = original(i,2);
- x_ = transformada(i,1);
- y_ = transformada(i,2);
- A(2*i-1,:) = [x,y,1,0,0,0,-x*x_,-y*x_];
- A(2*i,:) = [0,0,0,x,y,1,-x*y_,-y*y_];
-endfor
-x = inv(A'*A)*A'*L;
-x = [x;1];
-sizeT = (lin*col+1)/3;
-T = reshape(x,3,sizeT)';
+function T = find_transform(original,transformada)
+    [lin,col] = size(transformada);
+    transformada_visivel = [transformada(1,:);transformada(2,:)]
+    L = reshape(transformada_visivel,col*(lin-1),1);
+    for i = 1:col
+       x = original(1,i);
+       y = original(2,i);
+       x_ = transformada(1,i);
+       y_ = transformada(2,i);
+       A(2*i-1,:) = [x,y,1,0,0,0,-x*x_,-y*x_];
+       A(2*i,:) = [0,0,0,x,y,1,-x*y_,-y*y_];
+    endfor
+    x = inv(A'*A)*A'*L;
+    x = [x;1];
+    sizeT = (col*lin+1)/3;
+    T = reshape(x,3,3)';
+endfunction
