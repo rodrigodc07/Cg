34f457fe23dd8f0d114b531a68bf9070e62814ea
Rodrigo Cruz <rodrigo.cruz@b2wdigital.com>
2018-11-27 09:33:52 -0200

    Aumenta resolução

--- Trabalho 3/Ex3.m
+++ Trabalho 3/Ex3.m
@@ -1,32 +1,33 @@
-original = [135 493 520 111; 12 17 271 267; 1 1 1 1];
-transformada = [1 300 300 1; 1 1 300 300; 1 1 1 1]; 
+original = [135 500 520 110; 10 25 270 265; 1 1 1 1];
+transformada = [1 600 600 1; 1 1 600 600; 1 1 1 1]; 
 
 placa = imread('Placa_de_Trânsito_PARE.jpg');
-figure("Name","Imagem Original");
+figure();
 imshow(placa);
 
 T = inv(find_transform(original,transformada));
-new_img_1 = zeros(300,300,3,"uint8");
-new_img_2 = zeros(300,300,3,"uint8");
+constante = zeros(600,600,3,"uint8");
+triangular = zeros(600,600,3,"uint8");
 
-for c = 1:300 
-	for l = 1:300
-		pt = T * [ c; l; 1];
-		x = pt(1)/pt(3);
-		y = pt(2)/pt(3);
-		for i = 1:3
-			new_img_1(l,c,i) = placa(round(y),round(x),i);
-      w1 = placa(floor(y), floor(x), i) * (abs(x-floor(x))) * (abs(y-floor(y)));
-      w2 = placa(ceil(y), floor(x), i)* (abs(x-floor(x)))* (abs(y-ceil(y)));
-      w3 = placa(ceil(y), ceil(x), i)* (abs(x-ceil(x)))* (abs(y-ceil(y)));
-      w4 = placa(floor(y), ceil(x), i)* (abs(x-ceil(x)))* (abs(y-floor(y)));
-      new_img_2(l,c,i) = w1 + w2 + w3 + w4;
-		endfor
+for i = 1:600 
+	for j = 1:600
+    ponto = [i; j; 1];
+		novo_ponto = apply_transform(ponto, T);
+		x = novo_ponto(1);
+		y = novo_ponto(2);
+    for k = 1:3
+      constante(j,i,k) = placa(round(y),round(x),k);
+      w1 = placa(floor(y), floor(x), k) * (abs(x-floor(x))) * (abs(y-floor(y)));
+      w2 = placa(ceil(y), floor(x), k)* (abs(x-floor(x)))* (abs(y-ceil(y)));
+      w3 = placa(ceil(y), ceil(x), k)* (abs(x-ceil(x)))* (abs(y-ceil(y)));
+      w4 = placa(floor(y), ceil(x), k)* (abs(x-ceil(x)))* (abs(y-floor(y)));
+      triangular(j,i,k) = w1 + w2 + w3 + w4;	
+     endfor
 	endfor		
 endfor
 
-figure("Name","Imagem com núcleo constante");
-imshow(new_img_1);
+figure();
+imshow(constante);
 
-figure("Name","Imagem com núcleo trianguar");
-imshow(new_img_2);
+figure();
+imshow(triangular);
